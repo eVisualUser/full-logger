4779c68a2f11523ad50d82c46348b02546007413
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2023-03-22 09:45:36 +0100

    Added test

--- full_logger/examples/example.rs
+++ full_logger/examples/example.rs
@@ -1,7 +1,7 @@
-use full_logger::{file_manager::FileManager, logger::*};
+use full_logger::{file_manager::{FileManager, FileSize}, logger::*};
 
 fn main() {
-    let mut file_manager = FileManager::new(String::from("log"), 1000000);
+    let mut file_manager = FileManager::new(String::from("log"), FileSize::Mo(100));
     file_manager.set_file_prefix_str("DEMO_");
     file_manager.set_file_suffix_str("_LOG");
     file_manager.set_file_extension_str("log");
@@ -9,9 +9,13 @@ fn main() {
     let file = file_manager.get_file_path();
 
     set_file_format(FileFormat::CSV);
+    set_allow_console_log(true);
+    set_or_create_global_log_file("log", FileSize::Mo(100));
 
-    log(&file, vec!["error"], "Test");
+    simple_log(vec!["error"], "Test").unwrap();
+    log(&file, vec!["error"], "Test").unwrap();
 
     let result: Result<&str, &str> = Ok("Test");
+    simple_log_result(vec!["error"], result).unwrap();
     log_result(&file, vec!["error"], result).unwrap();
 }
