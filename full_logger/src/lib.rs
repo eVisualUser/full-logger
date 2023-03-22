85258ecbd55b98b8463afbfdc9a03980967fe401
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2023-03-22 09:46:14 +0100

    Added test

--- full_logger/src/lib.rs
+++ full_logger/src/lib.rs
@@ -1,3 +1,28 @@
 pub mod file_manager;
-pub mod log;
 pub mod logger;
+
+#[cfg(test)]
+pub mod test {
+    use crate::{file_manager::{FileManager, FileSize}, logger::*};
+
+    #[test]
+    pub fn global_test() {
+        let mut file_manager = FileManager::new(String::from("log"), FileSize::Mo(100));
+        file_manager.set_file_prefix_str("DEMO_");
+        file_manager.set_file_suffix_str("_LOG");
+        file_manager.set_file_extension_str("log");
+
+        let file = file_manager.get_file_path();
+
+        set_file_format(FileFormat::CSV);
+        set_allow_console_log(true);
+        set_or_create_global_log_file("log", FileSize::Mo(100));
+
+        simple_log(vec!["error"], "Test").unwrap();
+        log(&file, vec!["error"], "Test").unwrap();
+
+        let result: Result<&str, &str> = Ok("Test");
+        simple_log_result(vec!["error"], result).unwrap();
+        log_result(&file, vec!["error"], result).unwrap();
+    }
+}
